32b35df475d66e2accb1646b2249523e92aeb1ac
Vykad <vykad31@gmail.com>
2016-10-07 10:50:34 +0200

    Update Plateau.java

--- src/tetris/Plateau.java
+++ src/tetris/Plateau.java
@@ -5,5 +5,25 @@ public class Plateau
     private static final int screenWidth = 500;
     private static final int screenHeight = 500;
     private static final int mouvVertical = 25;
-    private static final int mouvLateral = 25;    
+    private static final int mouvLateral = 25;
+    
+    public int getScreenWidth()
+    {
+        return this.screenWidth;
+    }
+
+    public int getScreenHeight()
+    {
+        return this.screenHeight;
+    }
+
+    public int getMouvVertical()
+    {
+        return this.mouvVertical;
+    }
+
+    public int getMouvLateral()
+    {
+        return this.mouvLateral;
+    }    
 }

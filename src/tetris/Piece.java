dd99dd6234dfa14feb9fe9fe0a021ea0a1e07cd5
Vykad <vykad31@gmail.com>
2016-10-07 10:32:47 +0200

    Update Piece.java

--- src/tetris/Piece.java
+++ src/tetris/Piece.java
@@ -2,6 +2,32 @@ package tetris;
 
 import tetris.Cellule;
 
-public class Piece {
+public class Piece 
+{
+    private static final int screenWidth = 500;
+    private static final int screenHeight = 500;
+    private static final int mouvVertical = 25;
+    private static final int mouvLateral = 25;
     
+    private int posX;
+    private int posY;
+        
+    public Piece()
+    {
+        this.posX = 0;
+        this.posY = 0;
+    }
+    
+    public void versLeBas()
+    {
+        this.posY += mouvVertical;
+    }
+    public void versLaGauche()
+    {
+        this.posX -= mouvLateral;
+    }
+    public void versLaDroite()
+    {
+        this.posX += mouvLateral;
+    }
 }
